19fa395904de4de46cb22b67f501ea6e2044267e
Víctor Bettachini <victorb@gmx.net>
2025-09-27 06:40:40 -0300

    02Energía/pénduloFast.ipynb

--- 02Energía/funciones.py
+++ 02Energía/funciones.py
@@ -1,3 +1,6 @@
+import sympy as sm
+
+
 def energíaCinéticaTraslación(masa, posición, marcoDeReferencia):
     r"""
     A partir de la masa y posición de una partícula puntual en un marco de referencia devuelve su energía cinética.
@@ -25,6 +28,7 @@ def energíaCinéticaTraslación(masa, posición, marcoDeReferencia):
         ).simplify()
     return T_traslación
 
+
 def energíaPotencialGravitatoria(masa, posición, aceleracionGravitatoria):
     r"""
     Retorna la energía potencial gravitatoria de una partícula de masa m cuya posición r está dada en un sistema de referencia en el cual g es la aceleración gravitatoria terrestre.
@@ -50,3 +54,27 @@ def energíaPotencialGravitatoria(masa, posición, aceleracionGravitatoria):
         - (masa* aceleracionGravitatoria).dot(posición)
         ).simplify()
     return V_gravitatoria
+
+
+def energíaCinéticaRotación(momentoDeInercia, velocidadAngular):
+    """
+    Calcula la energía cinética de un cuerpo extenso (no puntual) que rota en torno a un eje.
+
+    Parámetros
+    ----------
+    momentoDeInercia : Multiplicación Sympy (sympy.core.mul.Mul)
+        Expresada en referencia al eje de rotación para la velocidadAngular.
+    velocidadAngular : Multiplicación Sympy (sympy.core.mul.Mul)
+        Velocidad de rotación en torno a un eje.
+
+    Retorna
+    -------
+    Igualdad Sympy (sympy.Equality)
+        Energía cinética de rotación (I/2)* omega**2
+    """
+    unMedio = sm.Rational(1,2) # Rational: fracción de enteros, alternativamente podría haberse usado 0.5
+    T_rotación = sm.Eq(
+        sm.Symbol('T_{rotación}'),
+        unMedio* momentoDeInercia* velocidadAngular**2
+     ).simplify()
+    return T_rotación
